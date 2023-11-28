7e92660c31e79c42317fb8a0b69f7a78273869fe
fihockey <fiona.hockey98@gmail.com>
2023-11-28 17:06:37 +0000

    Configured TS, first draft. Buttons click and show on screen, no maths happens but that is next. Some slight tweaks to SCSS.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,90 @@
-import './main.scss'
+import "./main.scss";
+
+let num1 = "";
+let operator = "";
+let num2 = "";
+let result = "";
+
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector(".clear-button");
+const dotButton = document.querySelector(".dot-button");
+const equalsButton = document.querySelector(".equals-button");
+const calculatorScreen = document.querySelector<HTMLHeadElement>(".calculator__grid-screen");
+
+if (
+  !numberButtons ||
+  !operatorButtons ||
+  !clearButton ||
+  !dotButton ||
+  !equalsButton ||
+  !calculatorScreen
+) {
+  throw new Error("Issue with button");
+}
+
+const handleClickNumber = (event: Event) => {
+  const button = event.target as HTMLButtonElement;
+  calculatorScreen.innerText += button.innerText;
+  if (!operator) {
+    num1 += button.innerText
+  } else {
+    num2 += button.innerText
+  }
+};
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", handleClickNumber);
+});
+
+// ADD
+const handleAdd = (num1 : number, num2 : number) => {
+  const sum = num1 + num2
+  return sum
+}
+
+// SUBTRACT
+const handleSubtract = (num1 : number, num2 : number) => {
+  const sum = num1 - num2
+  return sum
+}
+
+// MULTIPLY
+const handleMultiply = (num1 : number, num2 : number) => {
+  const sum = num1 * num2
+  return sum
+}
+
+// DIVIDE
+const handleDivide = (num1 : number, num2 : number) => {
+  const sum = num1 / num2
+  return sum
+}
+
+
+// EQUALS
+
+
+
+const handleClickOperator = (event: Event) => {
+  const button = event.target as HTMLButtonElement;
+  calculatorScreen.innerText += button.innerText;
+
+};
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", handleClickOperator);
+});
+
+const handleClickClear = (event: Event) => {
+  calculatorScreen.innerText = "";
+};
+
+clearButton.addEventListener("click", handleClickClear);
+
+const handleClickDot = (event: Event) => {
+  const button = event.target as HTMLButtonElement;
+  calculatorScreen.innerText += button.innerText;
+};
+
+dotButton.addEventListener("click", handleClickDot);
